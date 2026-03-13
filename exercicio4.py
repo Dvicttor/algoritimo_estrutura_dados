64604eae4f715ff53bab78643e4d88d5dc3a486c
Dvicttor <victtor2210@gmail.com>
2026-03-13 08:33:56 -0400

    novo exercicio

---
+++ exercicio4.py
@@ -0,0 +1,10 @@
+a = float(input("digite o primeiro lado:"))
+b = float(input("digite o segundo lado:"))
+c = float(input("digite o terceiro lado:"))
+
+lados = ([a,b,c])
+
+if a == b and b == c:
+ print("o triangulo e equilatero")
+else:
+ print("o triangulo nao e equilatero")
